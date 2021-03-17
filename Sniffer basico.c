4913b4ca5254da557aad05a7694d1d4f5ca87d55
Daniel Gonzalez <enda0507@gmail.com>
2021-03-17 00:03:12 -0600

    Se le da la funcionalidad del problema planteado, mostrando una solución con un solo proceso.
    Checar si es correcta la inicializacion del socket promiscuo.

--- Sniffer basico.c
+++ Sniffer basico.c
@@ -21,29 +21,83 @@ char buffer[MAXIM];
 int num;
 int capturas = 0;
 char red[MAXLINE];
+struct ethhdr *header;
+int ipv4=0, ipv6=0, arm=0, cdf=0, mac=0, ethernet=0, ieee=0;
 
 void display_packet( char *buf, int n )
 {
 	unsigned char	ch;
+	header = (struct ethhdr *)buffer;
+	
+	unsigned char des[6], src[6];
+	__be16 proto = ntohs(header->h_proto);
 
-	printf( "\npacket #%d ", ++num );
-	for (int i = 0; i < n; i+=16)
-		{
-		printf( "\n%04X: ", i );
-		for (int j = 0; j < 16; j++)
-			{
-			ch = ( i + j < n ) ? buf[ i+j ] : 0;
-			if ( i + j < n ) printf( "%02X ", ch );
-			else	printf( "   " );
-			}
-		for (int j = 0; j < 16; j++)
-			{
-			ch = ( i + j < n ) ? buf[ i+j ] : ' ';
-			if (( ch < 0x20 )||( ch > 0x7E )) ch = '.';
-			printf( "%c", ch );
-			}
+	
+
+	printf( "Paquete #%d\n", ++num );
+	
+	if(proto < 0x05DC && proto > 0x0000){
+		ieee++;
+		printf("IEEE 802.3\n");	
+	}
+	else if(proto >= 0x05DC){
+		ethernet++;
+		printf("Ethernet II\n");
+
+		printf("Destino: ");
+
+		for(int i = 0; i < 6; i++){
+			des[i] = header->h_dest[i];
+			printf("%02X ",des[i]);
 		}
-	printf( "\n%d bytes read\n-------\n", n );
+
+		printf("\nFuente: ");
+		for(int i = 0; i < 6; i++){
+			src[i] = header->h_source[i];
+			printf("%02X ",src[i]);
+		}
+
+		printf( "\nLongitud de trama: %d bytes\n", n);
+
+		printf( "Longitud carga util: %d bytes\n", n-14);
+
+		printf("Proto: %04X",proto);
+		if(proto == 0x0800){
+			ipv4++;	
+		}
+		else if(proto == 0x86dd){
+			ipv6++;
+		}
+		else if(proto == 0x0806){
+			arm++;
+		}
+		else if(proto == 0x8808){
+			cdf++;
+		}
+		else if(proto == 0x88E5){
+			mac++;
+		}
+
+		int d = des[0] & 0x01;
+		printf("\nDestino: ");
+		if(d == 0x01){
+			printf("Multicast\n");	
+		}
+		else if(d == 0x00){
+			printf("Unicast\n");
+		}
+
+		int s = src[0] & 0x01;
+		printf("Fuente: ");
+		if(s == 0x01){
+			printf("Multicast\n");	
+		}
+		else if(s == 0x00){
+			printf("Unicast\n");
+		}
+	} 
+
+	printf( "-------------------\n");
 }
 
 int main(){
@@ -79,11 +133,24 @@ int main(){
 
 	
 	printf("\nMonitoring all packet on interface \'%s\' \n",red);
-
+	
 	do{
-		int n = recvfrom(s, (char *)buffer, MAXLINE, 0, NULL, NULL);
+		int n = recvfrom(s, (char *)buffer, MAXIM, 0, NULL, NULL);
 		display_packet( buffer, n );
 	}while(num < capturas);
 
+	printf("IPv4: %d\n",ipv4);
+	printf("IPv6: %d\n",ipv6);
+	printf("ARM: %d\n",arm);
+	printf("CDF: %d\n",cdf);
+	printf("MAC: %d\n",mac);
+
+	printf( "-------------------\n");	
 	
+	printf("Ethernet II: %d\n",ethernet);
+	printf("IEEE 802.3: %d\n",ieee);
+	printf("Total analizado: %d\n",ethernet + ieee); 
+
+
+	system("/sbin/ifconfig enp0s3 -promisc");
 }
